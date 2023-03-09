3c8e61c35638431ac464413ebe2a5138d63a3c88
prem <premkattamanchi@gmail.com>
2023-03-09 22:16:48 +0530

    saved

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -1,6 +1,7 @@
 package com.driver;
 
 import java.util.*;
+import java.sql.Date;
 
 import org.springframework.stereotype.Repository;
 
@@ -14,6 +15,7 @@ public class WhatsappRepository {
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
     private HashMap<String,User> user;
+    private HashMap<Integer,Message> messageMap;
     private int customGroupCount;
     private int messageId;
 
@@ -23,33 +25,34 @@ public class WhatsappRepository {
         this.senderMap = new HashMap<Message, User>();
         this.adminMap = new HashMap<Group, User>();
         this.user = new HashMap<>();
+        this.messageMap=new HashMap<>();
         this.customGroupCount = 0;
         this.messageId = 0;
     }
     public String createUser(String name, String mobile) throws Exception {
         if(user.containsKey(mobile))
             throw new Exception("User already exists");
-
         user.put(mobile,new User(name,mobile));
         return "SUCCESS";
     }
     public Group createGroup(List<User> users){
         String groupName="";
-        if(users.size()>=2){
             if(users.size()==2)
                 groupName=users.get(1).getName();
             else {
                 customGroupCount++;
                 groupName = "Group " + customGroupCount;
             }
-        }
-        Group group=new Group(groupName,users.size());
-        adminMap.put(group,users.get(0));
-        groupUserMap.put(group,users);
+            Group group=new Group(groupName,users.size());
+            adminMap.put(group,users.get(0));
+            groupUserMap.put(group,users);
         return group;
     }
     public int createMessage(String content){
         messageId++;
+        long millis=System.currentTimeMillis();
+        Date date =new Date(millis);
+        messageMap.put(messageId,new Message(messageId,content,date));
         return messageId;
     }
 
@@ -58,24 +61,25 @@ public class WhatsappRepository {
         //Throw "You are not allowed to send message" if the sender is not a member of the group
         //If the message is sent successfully, return the final number of messages in that group.
         String groupName=group.getName();
-        String senderName= sender.getMobile();
+        String senderMobile= sender.getMobile();
         List<User> l;
-        boolean a=false;
-        boolean b=false;
+        boolean isGroupFound=false;
+        boolean isSenderFound=false;
         for(Group g:groupUserMap.keySet()){
-            if(g.getName().equals(groupName))
-                a=true;
-        }
-        if(a==false)
-            throw new Exception("Group does not exist");
-        if(groupUserMap.containsKey(group)){
-            l=groupUserMap.get(group);
-            for(User u:l){
-                if(senderName.equals(u.getMobile()))
-                    b=true;
+            if(g.getName().equals(groupName)) {
+                isGroupFound=true;
+                l=groupUserMap.get(g);
+                for(User u:l){
+                    if(senderMobile.equals(u.getMobile())){
+                        isSenderFound=true;
+                        break;
+                    }
+                }
             }
         }
-        if(b==false)
+        if(isGroupFound==false)
+            throw new Exception("Group does not exist");
+        if(isSenderFound==false)
             throw new Exception("You are not allowed to send message");
         groupMessageMap.get(group).add(message);
         senderMap.put(message,sender);
